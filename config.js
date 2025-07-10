e433ec494cbb110481c397a66117fb3047eb19f6
robinsonsameul <robbinssam525@gmail.com>
2025-07-10 14:27:30 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+const MAINNET_RPC = 'https://muddy-autumn-mansion.solana-mainnet.quiknode.pro/fe29d83d33bafd437ef2875364c0ddaa7669d46f';
+const DEVNET_RPC = 'https://muddy-autumn-mansion.solana-devnet.quiknode.pro/fe29d83d33bafd437ef2875364c0ddaa7669d46f';
+
+// use production server below
+const BUNDLER_SERVER_URL = 'https://pumpfun-bundler-server.onrender.com/'
+
+// (only for development purpose)
+// const BUNDLER_SERVER_URL = 'http://localhost/'
+
+// DO NOT EDIT CODE BELOW
+const CREATE_AND_BUY_URL = BUNDLER_SERVER_URL + 'create-and-buy';
+const SELL_ALL_URL = BUNDLER_SERVER_URL + 'sell-all';
+const SELL_URL = BUNDLER_SERVER_URL + 'sell';
+const BUY_URL = BUNDLER_SERVER_URL + 'buy';
+const BUNDLER_FEES_URL = BUNDLER_SERVER_URL + 'bundler-fees';
+const JITO_TIP = 0.01;
